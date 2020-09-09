080375cb8b5fde2e02b3e28bb1fe4742168e2490
evan@boissonnot.fr <Bienvenue_2510?>
2020-09-09 10:22:13 +0200

    evo: prepare animations

---
+++ first-animations/src/app/animations/first-component/first-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css'],
+  animations: [
+    
+  ]
+})
+export class FirstComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
