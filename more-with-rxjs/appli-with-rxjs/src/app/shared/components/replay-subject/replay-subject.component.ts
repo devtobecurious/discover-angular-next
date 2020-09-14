1a2fad37a4413c6e93432b0d21f2785a68fd0f8a
Evan Boissonnot <evan@boissonnot.fr>
2020-09-14 17:39:39 +0200

    evo: replay subject

--- more-with-rxjs/appli-with-rxjs/src/app/shared/components/replay-subject/replay-subject.component.ts
+++ more-with-rxjs/appli-with-rxjs/src/app/shared/components/replay-subject/replay-subject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-replay-subject',
@@ -10,6 +11,17 @@ export class ReplaySubjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const subject$ = new ReplaySubject(2);
+
+    subject$.subscribe(item => console.log('replay subject', 1, item));
+
+    subject$.next('A');
+    subject$.next('B');
+    subject$.next('C');
+
+    subject$.subscribe(item => console.log('replay subject', 2, item));
+
+    subject$.next('D');
   }
 
 }
