1da932df5b203fc6cfd293a10dc8db5ee128d236
evan@boissonnot.fr <Bienvenue_2510?>
2020-09-14 22:54:30 +0200

    fix: Observable.create => new Observable

--- more-with-rxjs/appli-with-rxjs/src/app/shared/components/cold-or-hot/cold-or-hot.component.ts
+++ more-with-rxjs/appli-with-rxjs/src/app/shared/components/cold-or-hot/cold-or-hot.component.ts
@@ -15,7 +15,7 @@ export class ColdOrHotComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.obsRandom$ = Observable.create(observer => {
+    this.obsRandom$ = new Observable(observer => {
       observer.next(Math.random());
     });
     this.obsRandom$.subscribe(item => console.log('cold random 1', item));
@@ -23,12 +23,12 @@ export class ColdOrHotComponent implements OnInit {
 
     const rand = Math.random();
 
-    const obs$Hot = Observable.create(observer => {
+    const obs$Hot = new Observable(observer => {
       observer.next(rand);
     });
     obs$Hot.subscribe(item => console.log('hot random 1', item));
     obs$Hot.subscribe(item => console.log('hot random 2', item));
-    
+
 
     this.obs$ = from(['🍕', '🍪', '🍔', '🌭', '🍟'])
       .pipe(
@@ -37,7 +37,7 @@ export class ColdOrHotComponent implements OnInit {
           return `Miam ${val}!`;
         }));
     // this.obs2$ = zip(this.obs$, interval(1000)).pipe(map(item => item[0]));
-    this.obs2$ = this.obs$.pipe(concatMap(item => of(item).pipe(delay(1000)))); //interval(1000).pipe(concatMap(item => this.obs$));
+    this.obs2$ = this.obs$.pipe(concatMap(item => of(item).pipe(delay(1000)))); // interval(1000).pipe(concatMap(item => this.obs$));
 
     this.obs2$.subscribe(item => console.log('hot miam', item));
   }
