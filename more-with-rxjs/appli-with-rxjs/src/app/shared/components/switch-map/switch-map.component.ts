42d7e46c6cc6aeb922ddd586c77f9079b80ac3e9
evan@boissonnot.fr <Bienvenue_2510?>
2020-09-15 10:59:18 +0200

    evo: switch map

--- more-with-rxjs/appli-with-rxjs/src/app/shared/components/switch-map/switch-map.component.ts
+++ more-with-rxjs/appli-with-rxjs/src/app/shared/components/switch-map/switch-map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { delay, mergeMap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-switch-map',
@@ -10,6 +12,15 @@ export class SwitchMapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const getData = param => {
+      return of(`retrieved new data with param ${param}`).pipe(
+        delay(1000)
+      );
+    };
+
+    // not run
+    from([1, 2, 3, 4]).pipe(switchMap(item => getData(item)))
+    .subscribe(item => console.log('switchmap', item));
   }
 
 }
