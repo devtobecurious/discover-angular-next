1ae6e26ff74127c74d377abfa766db099b3446dd
evan@boissonnot.fr <Bienvenue_2510?>
2020-09-27 09:04:14 +0200

    evo: local import for pipes

--- localization/i18n/src/app/app.module.ts
+++ localization/i18n/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OneWookieComponent } from './features/wookies/one-wookie/one-wookie.component';
+
+import localefr from '@angular/common/locales/fr';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localefr, 'fr');
 
 @NgModule({
   declarations: [
@@ -14,7 +19,9 @@ import { OneWookieComponent } from './features/wookies/one-wookie/one-wookie.com
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- localization/i18n/src/app/features/wookies/one-wookie/one-wookie.component.ts
+++ localization/i18n/src/app/features/wookies/one-wookie/one-wookie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-one-wookie',
@@ -9,9 +9,11 @@ export class OneWookieComponent implements OnInit {
   logo: string;
   minutes = 1;
   gender: 'male' | 'female' | 'other' = 'male';
+  currentDate = new Date();
 
-
-  constructor() { }
+  constructor(@Inject(LOCALE_ID) locale: string){
+    console.log('locale', locale);
+  }
 
   ngOnInit(): void {
     this.logo = 'https://gtswiki.gt-beginners.net/decal/png/16/80/39/8502889562077398016_1.png';
