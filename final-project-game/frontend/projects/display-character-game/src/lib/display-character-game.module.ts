bf5009bb1e83f6cafd2b2c8e2014f5ee1f3d8c88
Eavn BOISSONNOT <evan@boissonnot.fr>
2020-11-13 11:16:35 +0100

    (fix): component is not valid : not exists

--- final-project-game/frontend/projects/display-character-game/src/lib/display-character-game.module.ts
+++ final-project-game/frontend/projects/display-character-game/src/lib/display-character-game.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { DisplayCharacterGameComponent } from './display-character-game.component';
-import { DroideComponent } from './core/models/droide/droide.component';
 
 
 
 @NgModule({
   declarations: [
     DisplayCharacterGameComponent,
-    DroideComponent
   ],
   imports: [
   ],
