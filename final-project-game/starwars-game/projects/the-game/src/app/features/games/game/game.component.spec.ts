1d4384a430011d9efb3a356aa6c34d808e35f5ca
Eavn BOISSONNOT <evan@boissonnot.fr>
2021-01-06 17:35:02 +0100

    feat(game): add game background

--- final-project-game/starwars-game/projects/the-game/src/app/features/games/game/game.component.spec.ts
+++ final-project-game/starwars-game/projects/the-game/src/app/features/games/game/game.component.spec.ts
@@ -16,10 +16,23 @@ describe('GameComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the current scene background', () => {
+    fixture.detectChanges();
+    const element = fixture.nativeElement;
+
+    expect(element).toBeTruthy();
+
+    const container = element.querySelector('div.game');
+    expect(container).toBeTruthy();
+
+    expect(container.style.backgroundImage).toBe('url("http://www.ewokzone.com/wp-content/uploads/2020/01/endor-1-550x350.jpg")');
+    expect(container.style.width).toBe('550px');
+    expect(container.style.height).toBe('350px');
   });
 });
