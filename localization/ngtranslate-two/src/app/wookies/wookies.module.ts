27a05d35b2f11268e8e9aa63a1d8e2210a7407fa
Evan BOISSONNOT <evan@boissonnot.fr>
2021-10-29 15:00:26 +0200

    feat(translate): extend lazy loading

--- localization/ngtranslate-two/src/app/wookies/wookies.module.ts
+++ localization/ngtranslate-two/src/app/wookies/wookies.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListWookiesComponent } from './list-wookies/list-wookies.component';
 import { WookiesRoutingModule } from './wookies-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 
@@ -11,7 +12,10 @@ import { WookiesRoutingModule } from './wookies-routing.module';
   ],
   imports: [
     CommonModule,
-    WookiesRoutingModule
+    WookiesRoutingModule,
+    TranslateModule.forChild({
+      extend: true
+    })
   ]
 })
 export class WookiesModule { }
