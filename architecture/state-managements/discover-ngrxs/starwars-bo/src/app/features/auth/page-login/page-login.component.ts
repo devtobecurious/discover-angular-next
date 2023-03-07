a69d9128a1a30ace7b2749c08c5842708f437b1d
Evan BOISSONNOT <evan@boissonnot.fr>
2023-03-07 18:40:15 +0100

    feat: prepare auth

---
+++ architecture/state-managements/discover-ngrxs/starwars-bo/src/app/features/auth/page-login/page-login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-page-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-login.component.html',
+  styleUrls: ['./page-login.component.css']
+})
+export class PageLoginComponent {
+
+}
