22422fd99997e14b16cd3cdc08aa49d9467be61e
Evan BOISSONNOT <evan@boissonnot.fr>
2024-04-29 17:05:32 +0200

    core: switch to jest core: add ngrxsignal

---
+++ versions/18/my-training-courses/projects/dtbc-backoffice/src/app/features/authentication/components/authenticate-form/authenticate-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuthenticateFormComponent } from './authenticate-form.component';
+
+describe('AuthenticateFormComponent', () => {
+  let component: AuthenticateFormComponent;
+  let fixture: ComponentFixture<AuthenticateFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AuthenticateFormComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AuthenticateFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ versions/18/my-training-courses/projects/dtbc-backoffice/src/app/features/authentication/components/authenticate-form/authenticate-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dtbc-authenticate-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './authenticate-form.component.html',
+  styleUrl: './authenticate-form.component.css'
+})
+export class AuthenticateFormComponent {
+
+}
