07e03d3e42e1f10969775802a684e90fd0827c25
Evan BOISSONNOT <evan@boissonnot.fr>
2024-05-23 17:22:58 +0200

    feat: zoneless presentaiton

---
+++ versions/18/zone-less/src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    //provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)]
+};
