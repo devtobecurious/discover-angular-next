299cef567ea1ed0b57d08e1241a980407f17bd9c
Evan BOISSONNOT <evan@boissonnot.fr>
2024-05-23 17:35:26 +0200

    feat: coalescing to true

--- versions/18/coalescing-true/src/app/app.config.ts
+++ versions/18/coalescing-true/src/app/app.config.ts
@@ -1,11 +1,10 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)]
 };
