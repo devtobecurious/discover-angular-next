93413cb98a8b4f8373542c20954b75600da228b5
Evan BOISSONNOT <evan@boissonnot.fr>
2024-06-25 12:04:05 +0200

    core: prepare jwt auth web api project

---
+++ versions/18/training-courses/backend/TrainingCourses/TrainingCourses.Web.Api.UI/Models/AuthDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrainingCourses.Web.Api.UI.Models
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        protected AuthDbContext()
+        {
+        }
+    }
+}
