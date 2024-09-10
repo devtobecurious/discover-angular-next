07f1a3cc4c8a8c72f88c90bf1fa5e29df9c8be9f
Evan BOISSONNOT <evan@boissonnot.fr>
2024-09-10 12:00:54 +0200

    feat: @let

--- versions/19/test-v19/src/app/test-second/test-second.component.ts
+++ versions/19/test-v19/src/app/test-second/test-second.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { delay, of, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-test-second',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './test-second.component.html',
   styleUrl: './test-second.component.css'
 })
 export class TestSecondComponent {
-
+  characters$ = of([{ id: 1, name: 'Bilbo'}, { id: 1, name: 'Grand pas'}]).pipe(delay(1500))
+  title$$ = signal('A middle journey')
 }
